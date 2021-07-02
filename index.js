758a454bc6b0843f8b05927c1f4d650d22bb038b
Ryan <ryan@thecpafirm.com>
2021-07-02 11:33:59 -0600

    scrape TV Data

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const Puppeteer = require('puppeteer')
+const Puppeteer = require('puppeteer');
 
-(async () =>{
+(async () => {
+    console.log('\n----- Amazon TV Web Scrape Results: -----\n')
 
-    let sourceURL = 'https://www.amazon.com/s?rh=n%3A172659%2Cp_72%3A4-'
+    let sourceURL = 'https://www.amazon.com/s?rh=n%3A172659%2Cp_72%3A4'
 
     let browser = await Puppeteer.launch()
     let page = await browser.newPage()
 
     await page.goto(sourceURL)
+
+    let tvRes = await page.evaluate(()=>{
+        let tvEntry = document.querySelectorAll('div[data-component-type="s-search-result"]')
+        tvTemp = []
+
+        tvEntry.forEach(async (item) =>{
+            let tvName = await item.querySelector('span[class="a-size-base-plus a-color-base a-text-normal"]').textContent
+            let tvPrice = await item.querySelector('span[class="a-offscreen"]').textContent
+            let itemPush = {name: tvName, price: tvPrice}
+            tvTemp.push(itemPush)
+        })
+        return tvTemp
+    })
     await browser.close()
 
-})
+    console.log(tvRes)
+})()
